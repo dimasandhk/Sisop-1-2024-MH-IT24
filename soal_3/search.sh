09e6e7b86ec57160521a2a86336fc7d11fce1a01
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2024-03-25 15:45:00 +0700

    edit: search.sh

--- soal_3/search.sh
+++ soal_3/search.sh
@@ -2,25 +2,23 @@
 
 # C D E
 for category in genshin_character/*; do
-    if [ -d "$category" ]; then
-        for img in "$category"/*; do
-            fname=$(echo "$img" | awk -F '/' '{print $3}')
-            char=$(echo "$fname" | awk -F ' -' '{print $2}')
-            steghide extract -sf "$img" -p "" -xf "${char}.txt"
+    for img in "$category"/*; do
+        fname=$(echo "$img" | awk -F '/' '{print $3}')
+        char=$(echo "$fname" | awk -F ' -' '{print $2}')
+        steghide extract -sf "$img" -p "" -xf "${char}.txt"
 
-            decoded=$(cat "${char}.txt" | base64 -d)
+        decoded=$(cat "${char}.txt" | base64 -d)
 
-            echo "$fname -> $(cat "${char}.txt")"
-            if [[ "$decoded" == *http* ]]; then
-                wget "$decoded"
-                echo "$(date +"%Y/%m/%d %H:%M:%S") FOUND $img" >> image.log
-                echo "$decoded" > "${char}.txt"
-                exit 0
-            else
-                echo "$(date +"%Y/%m/%d %H:%M:%S") NOT FOUND $img" >> image.log
-                rm "${char}.txt"
-            fi
-            sleep 1
-        done
-    fi
+        echo "$fname -> $(cat "${char}.txt")"
+        if [[ "$decoded" == *http* ]]; then
+            wget "$decoded"
+            echo "$(date +"%Y/%m/%d %H:%M:%S") FOUND $img" >> image.log
+            echo "$decoded" > "${char}.txt"
+            exit 0
+        else
+            echo "$(date +"%Y/%m/%d %H:%M:%S") NOT FOUND $img" >> image.log
+            rm "${char}.txt"
+        fi
+        sleep 1
+    done
 done
